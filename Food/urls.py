9983cb49c9aa64b53abbd076296264d9d7f7e633
ghuynguyen <giahuynguyen2k@gmail.com>
2020-06-23 21:28:25 +0700

    implement view food info

--- Food/urls.py
+++ Food/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import FoodView
 urlpatterns = [
     path('', FoodView.homepageView),
-    path('search/', FoodView.searchByName, name='urlname')
+    path('search/', FoodView.searchByName, name='search_food_by_name'),
+    path('food/<int:food_id>/', FoodView.viewFoodInfo, name='view_food_info'),
 ]
--- Food/views.py
+++ Food/views.py
@@ -13,3 +13,7 @@ class FoodView(View):
         result = Food.objects.filter(name__contains=request.GET['food_name'])
         return render(request, 'search_page.html', {'food_list' : result})
 
+    def viewFoodInfo(request, food_id):
+        food = Food.objects.get(id=food_id)
+        return render(request, 'food_page.html', {'food': food})
+
