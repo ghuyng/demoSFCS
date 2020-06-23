31595e5dda79da2cb9c53aa3ee4637c0f8ec9e47
ghuynguyen <giahuynguyen2k@gmail.com>
2020-06-23 22:02:52 +0700

    improve food page

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('Food.urls')),
+    path('', include('Food.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
