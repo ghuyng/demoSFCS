df978f833d8893aba231fb51dae7e56283a9566a
ghuynguyen <giahuynguyen2k@gmail.com>
2020-06-24 01:34:08 +0700

    add function to signup

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Food.apps.FoodConfig'
+    'Food.apps.FoodConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
