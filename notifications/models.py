a9a48fabbd5d13cacbf246c4d371cda6be08157c
ghuynguyen <giahuynguyen2k@gmail.com>
2020-08-06 19:33:04 +0700

    add notifications UI and feature auto-reload store_order list when new order comming (using polling)

--- notifications/models.py
+++ notifications/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 
-from .signals import store_completed_signal
+from .signals import store_completed_signal, user_order_completed_signal
 from django.dispatch import receiver
 
 from store.models import Store
+from order.models import Status, StoreOrder
 # Create your models here.
 
 @receiver(store_completed_signal, sender=Store)
 def handler_store_completed(sender, **kwargs):
+    store_order = kwargs['store_order']
+    if store_order.order.getStatus() == Status.COMPLETED :
+        #signal user
+        user_order_completed_signal.send(order=store_order.order)
+
+
+@receiver(user_order_completed_signal)
+def handler_user_order_completed(**kwargs):
+    order = kwargs['order']
+    user = order.customer
     pass
+    #user.receive_noti(message)
--- notifications/signals.py
+++ notifications/signals.py
@@ -1,3 +1,4 @@
 from django.dispatch import Signal
 
 store_completed_signal = Signal(providing_args=["store_order"])
+user_order_completed_signal = Signal(providing_args=["order"])
