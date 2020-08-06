2ea6a9ac42a99efe5534e34c3a0ce18aa2f47a67
ghuynguyen <giahuynguyen2k@gmail.com>
2020-08-06 21:55:10 +0700

    add model notification

--- notifications/models.py
+++ notifications/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 
 from store.models import Store
 from order.models import Status, StoreOrder
+from django.contrib.auth.models import User
 # Create your models here.
 
 @receiver(store_completed_signal, sender=Store)
@@ -19,5 +20,18 @@ def handler_store_completed(sender, **kwargs):
 def handler_user_order_completed(**kwargs):
     order = kwargs['order']
     user = order.customer
-    pass
-    #user.receive_noti(message)
+    user.notification_set.create(message='Đơn hàng của bạn đã được hoàn tất', url_link=order.id)
+
+
+class Notification(models.Model):
+    customer = models.ForeignKey(User, on_delete = models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    message = models.CharField(max_length = 256) #message to display
+    url_link = models.CharField(max_length = 2000) #link the notification to some page
+
+    READ = 1
+    UNREAD = 0
+    NOTI_STATUS = [(UNREAD, 'unread'), (READ, 'read')]
+    status = models.BooleanField(choices=NOTI_STATUS, default=UNREAD, blank=False)
+
+
